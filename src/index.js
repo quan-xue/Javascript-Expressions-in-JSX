a01898a3e4ab6ef0524c91846a0d5852ed5394ed
Quan Xue <33763102+quan-xue@users.noreply.github.com>
2020-10-03 08:57:57 +0800

    Add javascript within html element Use {}

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Quan";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}!</h1>
+    <p>My lucky number is {Math.floor(Math.random() * 10)}.</p>
+  </div>,
+  document.getElementById("root")
+);
